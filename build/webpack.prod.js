144ed4b578442880fff0ceff511265d5d5dada28
luojz <929747115@qq.com>
2023-05-06 12:30:35 +0800

    feat: 新增生产环境配置

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,3 +4,8 @@
  * @FilePath     : /react-cli/build/webpack.prod.js
  * @Description  : 生产环境
  */
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
+module.exports = merge(baseConfig, {
+  mode: "production", // 生产模式，会开启 tree-shaking 和压缩代码，以及其他优化
+});
