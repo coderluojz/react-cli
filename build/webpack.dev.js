8f4e8df764db6c0a9f38ae7dadfe9841637c13ba
luojz <929747115@qq.com>
2023-05-08 12:38:20 +0800

    feat: 添加控制台提示信息

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,23 +1,27 @@
 /*
  * @Author       : luojingzhou
  * @Date         : 2023-05-06 10:40:48
- * @FilePath     : \react-cli\build\webpack.dev.js
+ * @FilePath     : /react-cli/build/webpack.dev.js
  * @Description  : 开发环境
  */
 const path = require("path");
 const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 
 const resolve = (dir) => path.resolve(__dirname, dir);
 
-const port = 3000;
+const port = process.env.POST || 3000;
+const host = process.env.HOST || "localhost";
 
-module.exports = merge(baseConfig, {
+const devConfig = merge(baseConfig, {
   mode: "development", // 开发模式
   devtool: "eval-cheap-module-source-map", // 源码调试模式：eval 形式执行代码-定位到行代码-展示源代码中的错误位置
   devServer: {
     port,
+    host,
+    open: true,
     compress: false, // gzip 压缩，开发环境不开启，提升热更新速度
     hot: true, // 开启热更新
     historyApiFallback: true, // 解决 history 路由 404 问题
@@ -32,3 +36,12 @@ module.exports = merge(baseConfig, {
     new ReactRefreshWebpackPlugin(), // 添加热更新插件
   ],
 });
+
+devConfig.plugins.push(
+  new FriendlyErrorsWebpackPlugin({
+    compilationSuccessInfo: {
+      messages: [`Your application is running here: http://${host}:${port}`],
+    },
+  })
+);
+module.exports = devConfig;
